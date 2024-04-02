96e9a42f9ca2bac06b88473f0d2fa7ddea67d6d9
zz1qwop <84325395+zz1qwop@users.noreply.github.com>
2024-04-02 22:04:12 +0900

    CSS, JS: 스크롤 시 요소들이 위로 올라오는 듯한 애니메이션 주기

---
+++ src/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const callback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('scroll-in');
+    } else {
+      entry.target.classList.remove('scroll-in');
+    }
+  });
+};
+const option = {
+  threshold: 0.3,
+};
+
+const observer = new IntersectionObserver(callback, option);
+const scrollItmes = document.querySelectorAll('.scroll__item');
+scrollItmes.forEach((scrollItem) => {
+  observer.observe(scrollItem);
+});
